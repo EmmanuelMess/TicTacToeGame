43b2bcfc97675ae5dd69cfe92577132c64856de0
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-06-29 16:27:21 -0300

    Fix nullability

--- core/src/com/emmanuelmess/tictactoe/TicTacToeGame.kt
+++ core/src/com/emmanuelmess/tictactoe/TicTacToeGame.kt
@@ -7,8 +7,9 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class TicTacToeGame : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+    private lateinit var batch: SpriteBatch
+    private lateinit var img: Texture
+
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
@@ -17,13 +18,13 @@ class TicTacToeGame : ApplicationAdapter() {
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+        batch.dispose()
+        img.dispose()
     }
 }
